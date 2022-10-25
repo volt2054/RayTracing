d65219bb8fdd14eaccd7fdaad57f8a7dd4da250a
Ethan <volt@pop-os.localdomain>
2022-10-25 22:30:34 +0100

    finished basic image class

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-// Your First C++ Program
-
 #include <iostream>
+#include <fstream>
+
 
 class Image {
 private:
     int width;
     int height;
+    std::string name;
 
 public:
     int getWidth() { return width; }
@@ -14,14 +15,24 @@ public:
     int setWidth(int width) { this->width = width; } 
     int setHeight(int height) { this->height = height; } 
 
-    Image(int width, int height) {
+    Image(int width, int height, std::string name) {
         this->width = width;
         this->height = height;
+        this->name = name;
     }
+
+    void initImage() {
+        std::ofstream File(name);
+        File << "P3 " << width << " " << height << " 255\n";
+        File.close();
+    } // PPM FORMAT SPEC - magic number, width, height, color depth, raw pixel values
+    
+    
 };
 
 int main() {
-    Image image(500, 500);
+    Image image(500, 500, "img.ppm");
+    image.initImage();
     std::cout << "Hello World!";
     return 0;
 }
