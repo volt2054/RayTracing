a2f23af82e3c0d679864a0dfc54d7b6a87bfdb1a
Ethan <ethan.williamson.2006@gmail.com>
2022-10-25 23:12:38 +0100

    added vec3 class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
 #include <fstream>
 
+class vec3
+{
+private:
+    double x;
+    double y;
+    double z;
 
-class Image {
+public:
+    vec3(double x, double y, double z)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+
+    // Operator Overloading
+    vec3 operator+(const vec3 &v) const
+    {
+        return vec3(x + v.x, y + v.y, z + v.z);
+    }
+
+    vec3 operator-(const vec3 &v) const
+    {
+        return vec3(x - v.x, y - v.y, z - v.z);
+    }
+
+    vec3 operator*(const vec3 &v) const
+    {
+        return vec3(x * v.x, y * v.y, z * v.z);
+    }
+    vec3 operator*(const double &T) const
+    {
+        return vec3(x * T, y * T, z * T);
+    }
+
+    vec3 operator/(const vec3 &v) const {
+        return vec3(x / v.x, y / v.y, z / v.z);
+    }
+
+    vec3 operator/(const double &T) const
+    {
+        return vec3(x / T, y / T, z / T)
+    }
+
+};
+
+class Image
+{
 private:
     int width;
     int height;
@@ -12,25 +58,26 @@ public:
     int getWidth() { return width; }
     int getHeight() { return height; }
 
-    int setWidth(int width) { this->width = width; } 
-    int setHeight(int height) { this->height = height; } 
+    int setWidth(int width) { this->width = width; }
+    int setHeight(int height) { this->height = height; }
 
-    Image(int width, int height, std::string name) {
+    Image(int width, int height, std::string name)
+    {
         this->width = width;
         this->height = height;
         this->name = name;
     }
 
-    void initImage() {
+    void initImage()
+    {
         std::ofstream File(name);
         File << "P3 " << width << " " << height << " 255\n";
         File.close();
     } // PPM FORMAT SPEC - magic number, width, height, color depth, raw pixel values
-    
-    
 };
 
-int main() {
+int main()
+{
     Image image(500, 500, "img.ppm");
     image.initImage();
     std::cout << "Hello World!";
